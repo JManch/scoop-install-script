4ae6fdb4d4f30b08a925031c6d6e22b2a0d61299
JManch <61563764+JManch@users.noreply.github.com>
2022-06-27 18:43:58 +0100

    Create scoop-install.bat

---
+++ scoop-install.bat
@@ -0,0 +1,26 @@
+# buckets
+scoop bucket add extras
+scoop bucket add games
+scoop bucket add versions
+scoop bucket add nerdfonts
+scoop bucket add JManch https://github.com/JManch/scoop-bucket
+scoop bucket add chawyehsu_dorado https://github.com/chawyehsu/dorado
+
+# utils
+scoop install 7zip git openssh ffmpeg
+
+# programs
+scoop install thisiswin11 onecommander neovim everything roundedtb bulk-crap-uninstaller hwinfo
+scoop install chromium discord spotify chatterino7 obs-studio sublime-text hxd msiafterburner
+
+# games
+scoop install steam multimc iracing-config
+
+# programming
+scoop install vscode vcredist2022 dotnet-sdk
+scoop uninstall vcredist vcredist2005 vcredist2008 vcredist2010 vcredist2012 vcredist2013 vcredist2022 dnspyex
+
+# terminal
+scoop install windows-terminal starship powershell
+
+# fonts
